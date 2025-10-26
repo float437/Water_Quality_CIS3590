d6312978a5a94a6c7863a4b9aef2fbc56aaa295d
Rajiv <rajiv@Rajivs-MacBook-Pro.local>
2025-10-26 17:08:13 -0400

    Created dbClient to move clean csv files to DB

---
+++ dbClient.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import pandas as pd
+import os
+
+load_dotenv()
+
+# reading from my .env file
+MONGO_USER = os.environ.get("MONGO_USER")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
+MONGO_CLUSTER_URL = os.environ.get("MONGO_CLUSTER_URL")
+
+url = (f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTER_URL}/?retryWrites=true&w=majority&appName=WaterQuality")
+
+client = MongoClient(url)
+
+
+
+try:
+    # rawFilesDB = client["rawCSV"]
+    # cleanFilesDB = client["cleanedCSV"]
+
+    cleanFilesDB = client.get_database("water_quality_data")
+
+    clean_CSV = pd.read_csv("data/Clean_CSV/cleanedOctober7.csv")
+    clean_CSV = clean_CSV.to_dict('records')
+    result = cleanFilesDB.collection.insert_many(clean_CSV)
+    print(f"Successfully inserted {len(result.inserted_ids)} documents.")
+except Exception as e:
+    raise Exception("Unable to find the document due to the following error: ", e)
+
+# TODO: exporting Raw_CSV's into the DB 
+# TODO: exporting Cleaned CSV's into the DB
